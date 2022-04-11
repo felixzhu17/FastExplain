5de827e0d4186ca09de139d23ef9243fed4a828d
Felix Zhu <fzhu@atlassian.com>
2022-04-11 15:27:29 +1000

    Bump version: 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="modelflow",
-    version="0.0.6",
+    version="0.0.7",
     license="MIT",
     author="Felix Zhu",
     author_email="zhu.felix@outlook.com",
