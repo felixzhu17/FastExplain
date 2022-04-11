ab23871c30c956d738d1ed7cde52eff6722d9017
Felix Zhu <fzhu@atlassian.com>
2022-04-11 15:33:54 +1000

    minor update

--- modelflow/utils/colours.py
+++ modelflow/utils/colours.py
@@ -7,11 +7,11 @@ COLOURS = {
     "white": "#FFFFFF",
     "grey": "#C1C7D0",
     "red": "#FF5630",
-    "yellow": "FFAB00",
-    "green": "36B37E",
-    "purple": "6554C0",
-    "black": "091E42",
-    "light_grey": "EBECF0",
+    "yellow": "#FFAB00",
+    "green": "#36B37E",
+    "purple": "#6554C0",
+    "black": "#091E42",
+    "light_grey": "#EBECF0",
 }
 
 
