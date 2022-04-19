cd2b6968efc8e7708bbf43542bbd4c52e51e43c1
Felix Zhu <fzhu@atlassian.com>
2022-04-19 11:32:20 +1000

    Update datasets.py

--- FastExplain/datasets/datasets.py
+++ FastExplain/datasets/datasets.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 DF_PATH = os.path.dirname(os.path.abspath(__file__))
 TITANIC_CSV_PATH = os.path.abspath(os.path.join(DF_PATH, "titanic.csv"))
-TITANIC_URL = "https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv"
+TITANIC_URL = "https://raw.githubusercontent.com/felixzhu17/FastExplain/main/FastExplain/datasets/titanic.csv"
 
 
 def load_titanic_data():
