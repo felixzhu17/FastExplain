c25d71d8cb22893fee802473d4a1dfceb3d205ac
Felix Zhu <fzhu@atlassian.com>
2022-04-19 11:26:32 +1000

    d

--- FastExplain/datasets/datasets.py
+++ FastExplain/datasets/datasets.py
@@ -5,7 +5,12 @@ import pandas as pd
 
 DF_PATH = os.path.dirname(os.path.abspath(__file__))
 TITANIC_CSV_PATH = os.path.abspath(os.path.join(DF_PATH, "titanic.csv"))
+TITANIC_URL = "https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv"
 
 
 def load_titanic_data():
-    return pd.read_csv(TITANIC_CSV_PATH)
+    try:
+        return pd.read_csv(TITANIC_CSV_PATH)
+    except:
+        return pd.read_csv(TITANIC_URL)
+
--- FastExplain/models/algorithms/hypertuning.py
+++ FastExplain/models/algorithms/hypertuning.py
@@ -61,7 +61,6 @@ def prepare_model_class(model_fit_func):
 
     return func
 
-
 def _check_model_fit_func(model_fit_func: type):
     if hasattr(model_fit_func, "fit") and hasattr(model_fit_func, "predict"):
         return
