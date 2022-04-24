bb05ec632de1fb797e0cdd566caf3d0ac55cc950
felixzhu17 <80929644+felixzhu17@users.noreply.github.com>
2022-04-24 18:29:16 +1000

    Update shap.py

--- FastExplain/explain/shap.py
+++ FastExplain/explain/shap.py
@@ -96,7 +96,12 @@ class Shap:
         return shap.plots.force(self.shap_values[index], *args, **kwargs)
 
     def shap_summary_plot(
-        self, filter=None, index=None, shap_max_samples=None, *args, **kwargs
+        self,
+        filter: Optional[str] = None,
+        index: Optional[list[Any]] = None,
+        shap_max_samples: Optional[int] = None,
+        *args,
+        **kwargs,
     ):
         """
         Create a SHAP beeswarm plot, colored by feature values when they are provided.
@@ -130,7 +135,12 @@ class Shap:
         return shap.plots.beeswarm(self.shap_values[index], *args, **kwargs)
 
     def shap_dependence_plot(
-        self, col, filter=None, index=None, shap_max_samples=None, *args, **kwargs
+        self,
+        filter: Optional[str] = None,
+        index: Optional[list[Any]] = None,
+        shap_max_samples: Optional[int] = None,
+        *args,
+        **kwargs,
     ):
         """
         Create a SHAP dependence scatter plot, colored by an interaction feature.
@@ -165,30 +175,15 @@ class Shap:
             self.shap_values[index, col], color=self.shap_values[index], *args, **kwargs
         )
 
-    def shap_importance_plot(self, *args, **kwargs):
+    def shap_importance_plot(
+        self,
+        *args,
+        **kwargs,
+    ):
         """
         Create a bar plot of a set of SHAP values.
 
         Args:
-            filter (Optional[str], optional):
-                The query string to evaluate.
-                You can refer to variables
-                in the environment by prefixing them with an '@' character like
-                ``@a + b``.
-                You can refer to column names that are not valid Python variable names
-                by surrounding them in backticks. Thus, column names containing spaces
-                or punctuations (besides underscores) or starting with digits must be
-                surrounded by backticks. (For example, a column named "Area (cm^2)" would
-                be referenced as ```Area (cm^2)```). Column names which are Python keywords
-                (like "list", "for", "import", etc) cannot be used.
-                For example, if one of your columns is called ``a a`` and you want
-                to sum it with ``b``, your query should be ```a a` + b``.
-                For more information refer to https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.query.html.
-                Defaults to None.
-            index (Optional[list[Any]], optional):
-                Provide index for shap_values, based on Shap.xs with reset index. Defaults to None.
-            shap_max_samples (Optional[int], optional):
-                Maximum number of samples for plotting. If provided, sets the new default for other plots. If None, uses default from class. Defaults to None.
             *args, **kwargs:
                 Additional arguments for SHAP Bar Plots. See https://shap.readthedocs.io/en/latest/example_notebooks/api_examples/plots/bar.html#
         """
