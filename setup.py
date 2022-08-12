2995b54ebb89034fe6889920b3e7b8e1479aab3c
Felix Zhu <fzhu@atlassian.com>
2022-08-12 17:00:04 +1000

    Bump version: 0.0.61 → 0.0.62

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fast-explain",
-    version="0.0.61",
+    version="0.0.62",
     license="MIT",
     author="Felix Zhu",
     author_email="zhu.felix@outlook.com",
